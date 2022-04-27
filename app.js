46ff1bfe85c22c88d42a4e37642205e9372823c4
jenna-graham <jclayto2@gmail.com>
2022-04-27 12:25:35 -0700

    added button that picks a random item

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const pickSoda = document.getElementById('soda');
+const button = document.getElementById('button');
 
 pickSoda.addEventListener('click', () => {
     pickSoda.classList.toggle('picked');
@@ -26,4 +27,31 @@ const pickWatermelon = document.getElementById('watermelon');
 
 pickWatermelon.addEventListener('click', () => {
     pickWatermelon.classList.toggle('picked');
+});
+
+button.addEventListener('click', () => {
+    pickSoda.classList.remove('picked');
+    pickCheese.classList.remove('picked');
+    pickGrapes.classList.remove('picked');
+    pickBread.classList.remove('picked');
+    pickWatermelon.classList.remove('picked');
+
+    console.log('picking random food!');
+
+    var food = ['soda', 'cheese', 'grapes', 'bread', 'watermelon'];
+    let randomFood = food[Math.floor(Math.random() * 5)];
+    
+    console.log('random food added');
+
+    if (randomFood === 'soda') {
+        pickSoda.classList.add('picked');
+    } else if (randomFood === 'cheese') {
+        pickCheese.classList.add('picked');
+    } else if (randomFood === 'grapes') {
+        pickGrapes.classList.add('picked');
+    } else if (randomFood === 'bread') {
+        pickBread.classList.add('picked');
+    } else {
+        pickWatermelon.classList.add('picked');
+    }
 });
