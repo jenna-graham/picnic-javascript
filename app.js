d635f64a8ca55ca1247741112b3c09b82cbd8621
jenna-graham <jclayto2@gmail.com>
2022-04-27 11:41:25 -0700

    moved cheese to blanket

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const pickSoda =document.getElementById('soda');
+const pickSoda = document.getElementById('soda');
 
 pickSoda.addEventListener('click', () => {
     pickSoda.classList.toggle('picked');
+});
+
+const pickCheese = document.getElementById('cheese');
+
+pickCheese.addEventListener('click', () => {
+    pickCheese.classList.toggle('picked');
 });
